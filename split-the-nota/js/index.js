8022d3509c9b747877645c3ffb34790daf42c158
Maja Gedda <maja.gedda@zocom.se>
2020-11-12 13:11:49 +0100

    push

---
+++ split-the-nota/js/index.js
@@ -0,0 +1,13 @@
+import { calcSum } from './modules/operations.js';
+
+//skapa event listener på knappen
+
+//räkna ut den totala summan
+
+//dela på antalet vänner
+
+//returnera varderas summa
+
+document.getElementById('calculateButton').addEventListener('click', function() {
+    calcSum();
+})
---
+++ split-the-nota/js/modules/operations.js
@@ -0,0 +1,6 @@
+let calcSum = () => {
+    console.log('heyyo');
+    //returnera summan
+}
+
+export { calcSum };
