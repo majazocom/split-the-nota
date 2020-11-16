2a9246fb164283a2518810b70fd543d966128af9
Maja Gedda <maja.gedda@zocom.se>
2020-11-16 09:30:00 +0100

    solution

--- split-the-nota/js/index.js
+++ split-the-nota/js/index.js
@@ -1,13 +1,22 @@
 import { calcSum } from './modules/operations.js';
 
+let friends = 0;
+let tip = 0;
+let sum = 0;
+
+
 //skapa event listener på knappen
 
-//räkna ut den totala summan
-
-//dela på antalet vänner
-
-//returnera varderas summa
-
 document.getElementById('calculateButton').addEventListener('click', function() {
-    calcSum();
+
+    friends = document.getElementById('numberOfFriends').value;
+    tip = document.getElementById('tip').value;
+    sum = document.getElementById('sum').value;
+
+    const total = calcSum(sum, friends, tip);
+
+    document.getElementById('inputForm').classList.toggle('hide');
+    document.getElementById('showSum').classList.toggle('hide');
+    document.getElementById('friendSum').innerHTML = total;
 })
+
--- split-the-nota/js/modules/operations.js
+++ split-the-nota/js/modules/operations.js
@@ -1,6 +1,27 @@
-let calcSum = () => {
-    console.log('heyyo');
-    //returnera summan
+let calcSum = (sum, friends, tip) => {
+    console.log(sum, friends, tip);
+    //räkna ut den totala summan
+    const calculatedTip = calculateTip(sum, tip);
+    const total = parseInt(sum) + calculatedTip;
+
+    //dela på antalet vänner
+    const friendSum = divideTotal(total, friends);
+    //returnera varderas summa
+    console.log('each friend should pay: ' + friendSum)
+
+    return friendSum;
+}
+
+let divideTotal = (total, friends) => {
+    const sumDivided = total / parseInt(friends);
+    return sumDivided;
+}
+
+let calculateTip = (sum, tip) => {
+
+    const calculatedTip = parseInt(sum) * parseFloat(tip);
+
+    return calculatedTip;
 }
 
 export { calcSum };
